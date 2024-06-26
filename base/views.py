b2bd822f4e3f479ec52f21d0d35c23d20888e085
Alisher <dautovalisher33@gmail.com>
2024-06-26 23:15:26 +0500

    Import Task from Models

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
-
+from .models import Task
 
 # Create your views here.
 class TaskList(ListView):
