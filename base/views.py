663e6f0f05bd27e569211b66341edd1f1c013105
Alisher <dautovalisher33@gmail.com>
2024-06-28 23:12:42 +0500

    Create Class TaskDelete

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 
 # Create your views here.
@@ -25,3 +25,7 @@ class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
