cc87073be78d0b76e817fb83e454b56e838a1162
Alisher <dautovalisher33@gmail.com>
2024-06-29 23:18:57 +0500

    Add url to login/

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='detail'),
     path('create-task/', TaskCreate.as_view(), name='create'),
     path('update-task/<int:pk>/', TaskUpdate.as_view(), name='update'),
